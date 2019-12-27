47c425398592a6047d3d700dcac0b0091886afbd
Richa Maheshwari <rmaheshwari@awcsoftware.net>
2019-12-27 12:14:19 +0530

    removed the function getsetfieldvalue()

--- ServicePoInvoices.js
+++ ServicePoInvoices.js
@@ -95,114 +95,35 @@ function validate_ServicePoInvoices(pEvent,activityName)
 {		
 	switch(pEvent)
     {           
-        case 'I':
+      case 'I':
 		case 'D':
-		console.log("Introduce/Done");
-		let nameofprocess = document.getElementById('proctype').value;
-		if(nameofprocess==''||nameofprocess==null){
-		alert("Kindly select any Name of Process");
-		return false;
-		}
-		getsetFieldValue();
-		return true;
+			console.log("Introduce/Done");
+			console.log("testing");
+		//	let initiatorstatus = document.getElementById('initiatorstatus').value;
+				if(activityName=='Initiator'){
+					console.log("Initiator");
+	//		if(initiatorstatus==''){
+		//		com.newgen.omniforms.util.showError("","Kindly enter the decision");
+	//			return false;
+		//	}	
+			let nameofprocess = document.getElementById('proctype').value;
+			if(nameofprocess==''||nameofprocess==null){
+				alert("Kindly select any Name of Process");
+				return false;
+			}
+				}
 				
-		return true;
+			
+			
+				
+			
+			return true;
 		  
 		case 'S':
 		saveFormData(); 
 		console.log("Save");
-			//return true;
-		getsetFieldValue();
-		return true;			
+			return true;		
 	}
-}
-
-
-function getsetFieldValue(){
-					console.log("inside proctype");
-					switch(document.getElementById('proctype').value)
-					{
-					case 'Branding Out of home':
-					case 'Branding Relationship management':
-					case 'Branding Shop':
-					case 'Branding Tour Scheme':
-					case 'Branding Retailer Meeting':
-					case 'Technical Item':
-					case 'Technical Printing':
-					if(com.newgen.omniforms.formviewer.getNGValue("pannumber")==""){
-						alert("Enter the Value of PAN Number ");
-					}
-					if(com.newgen.omniforms.formviewer.getNGValue("invoicenumber")==""){
-						alert("Enter the Value of Invoice Number ");
-					}
-					if(com.newgen.omniforms.formviewer.getNGValue("invoicedate")==""){
-						alert("Enter the Value of Invoice Date");
-					}
-					if(com.newgen.omniforms.formviewer.getNGValue("Text11")==""){
-						alert("Enter the Value of Particular ");
-					}
-					if(com.newgen.omniforms.formviewer.getNGValue("Text13")==""){
-						alert("Enter the Value of Period");
-					}
-					if(com.newgen.omniforms.formviewer.getNGValue("Text15")==""){
-						alert("Enter the Value of Qty/Size");
-					}
-					if(com.newgen.omniforms.formviewer.getNGValue("Text16")==""){
-						alert("Enter the Value of Rate");
-					}
-					if(com.newgen.omniforms.formviewer.getNGValue("Text17")==""){
-						alert("Enter the Value of Amount");
-					}
-					if(com.newgen.omniforms.formviewer.getNGValue("totaltaxableamount")==""){
-						alert("Enter the Value of Total Taxable Amount ");
-					}
-					if(com.newgen.omniforms.formviewer.getNGValue("totalbillamount")==""){
-						alert("Enter the Value of Total Bill Amount");
-					}
-					break;
-					
-					case 'Branding Branding Expenses':
-					case 'Branding Mass Advertisement':
-					case 'Branding Sponsorship':
-					case 'Branding Dealer Meeting':
-					if(com.newgen.omniforms.formviewer.getNGValue("Text22")==""){
-						alert("Enter the Value of Qty/Size");
-					}
-					if(com.newgen.omniforms.formviewer.getNGValue("pannumber")==""){
-						alert("Enter the Value of PAN Number ");
-					}
-					if(com.newgen.omniforms.formviewer.getNGValue("invoicenumber")==""){
-						alert("Enter the Value of Invoice Number ");
-					}
-					if(com.newgen.omniforms.formviewer.getNGValue("invoicedate")==""){
-						alert("Enter the Value of Invoice Date");
-					}
-					if(com.newgen.omniforms.formviewer.getNGValue("totaltaxableamount")==""){
-						alert("Enter the Value of Total Taxable Amount ");
-					}
-					if(com.newgen.omniforms.formviewer.getNGValue("totalbillamount")==""){
-						alert("Enter the Value of Total Bill Amount");
-					}
-					if(com.newgen.omniforms.formviewer.getNGValue("Text18")==""){
-						alert("Enter the Value of Particular");
-					}
-					if(com.newgen.omniforms.formviewer.getNGValue("Text20")==""){
-						alert("Enter the Value of Period ");
-					}
-					if(com.newgen.omniforms.formviewer.getNGValue("Text21")==""){
-						alert("Enter the Value of Package");
-					}
-					if(com.newgen.omniforms.formviewer.getNGValue("Text25")==""){
-						alert("Enter the Value of Edition");
-					}
-					if(com.newgen.omniforms.formviewer.getNGValue("Text23")==""){
-						alert("Enter the Value of Rate ");
-					}
-					if(com.newgen.omniforms.formviewer.getNGValue("Text24")==""){
-						alert("Enter the Value of Amount");
-					}
-					break;
-					
-	
+return true;
 }
 		
