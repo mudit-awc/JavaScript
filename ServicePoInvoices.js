b86313164303c0665823b53e9ec163636540daab
Richa Maheshwari <rmaheshwari@awcsoftware.net>
2020-01-09 12:30:34 +0530

    changes the process names

--- ServicePoInvoices.js
+++ ServicePoInvoices.js
@@ -6,8 +6,8 @@ function eventDispatched_ServicePoInvoices(pId,pEvent){
         {
 			switch(pEvent.srcElement.id)
             {
-				case 'Btn_Add_linedetails1':
-				//console.log("inside button click og first line details");
+				//case 'Btn_Add_linedetails1':
+				/*console.log("inside button click og first line details");
 						if(com.newgen.omniforms.formviewer.getNGValue("Text11")==""){
 					    com.newgen.omniforms.util.showError("","Kindly enter the value of Particular");
 						return false;
@@ -27,15 +27,15 @@ function eventDispatched_ServicePoInvoices(pId,pEvent){
 					    if(com.newgen.omniforms.formviewer.getNGValue("Text17")==""){
 					    	com.newgen.omniforms.util.showError("","Kindly enter the value of Amount");
 						return false;
-					    }
+					    }*/
 					
-					return true;
+					//return true;
 			
-				case 'Btn_Delete_linedetails1':
-				return true;
+				//case 'Btn_Delete_linedetails1':
+				//return true;
 				
-				case 'Btn_Modify_linedetails1':
-				return true;
+				//case 'Btn_Modify_linedetails1':
+				//return true;
 				
 				case 'Btn_Add_linedetails2':
 					  if(com.newgen.omniforms.formviewer.getNGValue("Text18")==""){
@@ -66,6 +66,7 @@ function eventDispatched_ServicePoInvoices(pId,pEvent){
 					  	com.newgen.omniforms.util.showError("","Kindly enter the Qty/Size");
 						return false;
 					  }
+					  
 				
 				return true;
 			
@@ -95,34 +96,19 @@ function eventDispatched_ServicePoInvoices(pId,pEvent){
 				{
 					switch(document.getElementById('proctype').value)
 					{
-						case 'Branding Out of home':
-						case 'Branding Relationship management':
-						case 'Branding Shop':
-						case 'Branding Tour Scheme':
-						case 'Branding Retailer Meeting':
-						case 'Technical Item':
-						case 'Technical Printing':
-						console.log("");
+						case 'Branding - General':
+						case 'Branding – Out of home':
+						case 'Technical – PO':
+						case 'Project/Plant/Non Plant Operations':
+						case 'Tour Bill':
+						case 'Kind Scheme (Retailor Meeting)':
+						case 'Kind Scheme (Gold & Silver)':
+						case 'Branding –Out of home (Vehicle Painting At NBH)':
+						case 'Consultant for Marketing Development':
+						case 'Mobile Van Technical PO':
 						com.newgen.omniforms.formviewer.setVisible("Frame2", true);
-						com.newgen.omniforms.formviewer.setVisible("Frame4", false);
-						com.newgen.omniforms.formviewer.setVisible("Label7", false);
-						com.newgen.omniforms.formviewer.setVisible("sachsn", false);
-						com.newgen.omniforms.formviewer.setVisible("LINE_DETAILS", true);
-						com.newgen.omniforms.formviewer.setVisible("Frame8", true);
-						com.newgen.omniforms.formviewer.setTop("LINE_DETAILS","440px");
-						com.newgen.omniforms.formviewer.setTop("Frame8","820px");
-						com.newgen.omniforms.formviewer.setHeight("FRM_ServicePoInvoices","1150px");
-						return true;
-						
-						
-						case 'Branding Branding Expenses':
-						case 'Branding Mass Advertisement':
-						case 'Branding Sponsorship':
-						case 'Branding Dealer Meeting':
-						com.newgen.omniforms.formviewer.setVisible("Frame2", true);
-						com.newgen.omniforms.formviewer.setVisible("Label7", true);
-						com.newgen.omniforms.formviewer.setVisible("sachsn", true);
-						com.newgen.omniforms.formviewer.setVisible("LINE_DETAILS", false);
+						//com.newgen.omniforms.formviewer.setVisible("Label7", true);
+						//com.newgen.omniforms.formviewer.setVisible("sachsn", true);
 						com.newgen.omniforms.formviewer.setVisible("Frame4", true);
 						com.newgen.omniforms.formviewer.setVisible("Frame8", true);
 						com.newgen.omniforms.formviewer.setTop("Frame4","440px");
@@ -132,7 +118,6 @@ function eventDispatched_ServicePoInvoices(pId,pEvent){
 						
 						default:
 						com.newgen.omniforms.formviewer.setVisible("Frame2", false);
-						com.newgen.omniforms.formviewer.setVisible("LINE_DETAILS", false);
 						com.newgen.omniforms.formviewer.setVisible("Frame4", false);
 						com.newgen.omniforms.formviewer.setVisible("Frame8", false);
 						
@@ -171,10 +156,11 @@ function validate_ServicePoInvoices(pEvent,activityName)
 			if(nameofprocess==''||nameofprocess==null){
 				com.newgen.omniforms.util.showError("","Kindly enter the Process Name");
 						return false;
+
 			}
 			}	
 			var proctype_value = document.getElementById('proctype').value;
-			if(proctype_value == ("Branding Out of home"||"Branding Relationship management"||"Branding Shop"||"Branding Tour Scheme"||"Branding Retailer Meeting"||"Technical Item"||"Technical Printing")){
+			if(proctype_value == ("Branding - General"||"Branding – Out of home"||"Technical – PO"||"Project/Plant/Non Plant Operations"||"Tour Bill"||"Kind Scheme (Retailor Meeting)"||"Kind Scheme (Gold & Silver)"||"Branding –Out of home (Vehicle Painting At NBH)"||"Consultant for Marketing Development"||"Mobile Van Technical PO")){
 			    if(com.newgen.omniforms.formviewer.getNGValue("Text_pan1")==""){
 			    com.newgen.omniforms.util.showError("","Kindly enter the PAN Number");
 			    return false;
@@ -196,28 +182,9 @@ function validate_ServicePoInvoices(pEvent,activityName)
 			    return false;
 			    }
 			}
-			else{
-				if(com.newgen.omniforms.formviewer.getNGValue("invoicenumber")==""){
-				com.newgen.omniforms.util.showError("","Kindly enter the Invoice Number");
-				return false;
-					}
-				if(com.newgen.omniforms.formviewer.getNGValue("invoicedate")==""){
-				com.newgen.omniforms.util.showError("","Kindly enter the Invoice Date");
-				return false;
-					}
-				if(com.newgen.omniforms.formviewer.getNGValue("pannumber")==""){
-				com.newgen.omniforms.util.showError("","Kindly enter the PAN Number");
-				return false;
-					}	
-				if(com.newgen.omniforms.formviewer.getNGValue("totaltaxableamount")==""){
-				com.newgen.omniforms.util.showError("","Kindly enter the value of Total Taxable Amount");
-				return false;
-					}
-				if(com.newgen.omniforms.formviewer.getNGValue("totalbillamount")==""){
-				com.newgen.omniforms.util.showError("","Kindly enter the value of Total Bill Amount");
-				return false;
-					}
-				}
+			else{}
+				
+
 			break;
 			
 		case 'S':
@@ -226,7 +193,74 @@ function validate_ServicePoInvoices(pEvent,activityName)
 //return true;
 }
 
-         
+
+/*function getsetfieldvalues(){
+	
+					console.log("inside proctype of service po invoices");
+					switch(document.getElementById('proctype').value)
+					{
+						case 'Branding Out of home':
+						case 'Branding Relationship management':
+						case 'Branding Shop':
+						case 'Branding Tour Scheme':
+						case 'Branding Retailer Meeting':
+						case 'Technical Item':
+						case 'Technical Printing':
+							
+						if(com.newgen.omniforms.formviewer.getNGValue("pannumber")==""){
+						com.newgen.omniforms.util.showError("","Kindly enter the PAN Number");
+						return false;
+						}
+						if(com.newgen.omniforms.formviewer.getNGValue("invoicenumber")==""){
+						com.newgen.omniforms.util.showError("","Kindly enter the Invoice Number");
+						return false;
+						}
+					    if(com.newgen.omniforms.formviewer.getNGValue("invoicedate")==""){
+						com.newgen.omniforms.util.showError("","Kindly enter the Invoice Date");
+						return false;
+					    }
+					  
+					    if(com.newgen.omniforms.formviewer.getNGValue("totaltaxableamount")==""){
+					    com.newgen.omniforms.util.showError("","Kindly enter the value of Total Taxable Amount");
+						return false;
+					    }
+					    if(com.newgen.omniforms.formviewer.getNGValue("totalbillamount")==""){
+					    com.newgen.omniforms.util.showError("","Kindly enter the value of Total Bill Amount");
+						return false;
+					    }
+						break;
+						
+					  case 'Branding Branding Expenses':
+					  case 'Branding Mass Advertisement':
+					  case 'Branding Sponsorship':
+					  case 'Branding Dealer Meeting':
+					  if(com.newgen.omniforms.formviewer.getNGValue("invoicenumber")==""){
+					  	com.newgen.omniforms.util.showError("","Kindly enter the Invoice Number");
+						return false;
+					  }
+						if(com.newgen.omniforms.formviewer.getNGValue("invoicedate")==""){
+					  	com.newgen.omniforms.util.showError("","Kindly enter the Invoice Date");
+						return false;
+					  }
+					   if(com.newgen.omniforms.formviewer.getNGValue("pannumber")==""){
+					  	com.newgen.omniforms.util.showError("","Kindly enter the PAN Number");
+						return false;
+					  }
+					 
+					  if(com.newgen.omniforms.formviewer.getNGValue("totaltaxableamount")==""){
+					  	com.newgen.omniforms.util.showError("","Kindly enter the value of Total Taxable Amount");
+						return false;
+					  }
+					  if(com.newgen.omniforms.formviewer.getNGValue("totalbillamount")==""){
+						com.newgen.omniforms.util.showError("","Kindly enter the value of Total Bill Amount");
+						return false;
+					  }
+					  
+					  break;
+					  	
+					}              
+	} */                
+
 	                  
  
 		
